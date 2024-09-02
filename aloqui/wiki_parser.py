58a78564ef80f6c75bcadba3c0fa479af3d88413
ianwcooley <ianwcooley@gmail.com>
2024-09-02 00:49:39 -0500

    now finds only the section for the given language on the page for the given word. writes it in html to test.html and in plaintext to test.txt

--- aloqui/wiki_parser.py
+++ aloqui/wiki_parser.py
@@ -2,8 +2,9 @@ import webbrowser
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import quote
+import re
 import constants
-
+import pprint
 
 # NOTE: This function is not called anywhere.
 def check_and_open_url(url):
@@ -34,10 +35,12 @@ def get_soup(url):
         print(f"Failed to reach the URL: {e}")
     return soup
 
-def get_word_info(word, language):
+def get_html(word, language):
+    """Goes to the wiktionary page for the given word, and writes to test.html
+    only the section of the page's html code that corresponds to the given language"""
     encoded_word = quote(word)
     url = f"https://en.wiktionary.org/wiki/{encoded_word}#{language}"
-    # check_and_open_url(url) # for testing purposes
+    check_and_open_url(url) # NOTE: for testing purposes only
     soup = get_soup(url)
     if (soup):
         # Get div that contains heading with language name:
@@ -56,9 +59,52 @@ def get_word_info(word, language):
         # Print the collected elements
         with open('test.html', 'w') as file:
             for element in elements:
-                file.write('ELEMENT\n')
-                for part_of_speech in constants.PARTS_OF_SPEECH:
-                    if element.find(lambda tag: tag.get('id', '').startswith(part_of_speech)):
-                        file.write('PART OF SPEECH\n')
-                        print(element.get_text())
+                # file.write('ELEMENT\n')
+                # for part_of_speech in constants.PARTS_OF_SPEECH:
+                #     if element.find(lambda tag: tag.get('id', '').startswith(part_of_speech)):
+                #         file.write('PART OF SPEECH\n')
+                #         print(element.get_text())
                 file.write(element.prettify() + '\n')
+
+def get_plain_text(word, language):
+    # MediaWiki API endpoint for English Wiktionary
+    url = "https://en.wiktionary.org/w/api.php"
+
+    # Define the parameters for the API request
+    params = {
+        "action": "query",
+        "prop": "extracts",
+        "titles": word,
+        "format": "json",
+        "explaintext": True  # Ensures the extract is plain text
+    }
+
+    # Make the API request
+    response = requests.get(url, params=params)
+    
+    # Parse the JSON response
+    data = response.json()
+
+    # Regular expression for part of plain text corresponding to given language
+    language_section_pattern = rf"(== {language} ==[\s\S]*?)(?=\n== [^\s=][^=]* ==|\Z)"
+
+    # Extract the page content and write to file
+    with open('test.txt', 'w') as file:
+        pages = data.get("query", {}).get("pages", {})
+        for page_id, page_content in pages.items():
+            extract = page_content.get("extract", "No content found.")
+            match = re.search(language_section_pattern, extract, re.DOTALL)
+            if match:
+                language_section = match.group(1)  # Extract the content in the Spanish section
+                return file.write(language_section)
+            else:
+                return "No Spanish section found in the text."
+
+
+    # # Extract the page content
+    # pages = data["query"]["pages"]
+    # page_content = next(iter(pages.values()))["extract"]
+
+def get_word_info(word, language):
+    get_html(word, language)
+    get_plain_text(word, language)
