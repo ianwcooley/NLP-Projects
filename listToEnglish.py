8a711b59304b7703e7ff6154ddf3efd6475f1948
ianwcooley <ianwcooley@gmail.com>
2024-09-09 15:33:54 -0500

    Added ignore folder to ignore irrelevant files that I don't want to track

--- listToEnglish.py
+++
@@ -1,8 +0,0 @@
-def listToEnglish(l):
-    for i in range(len(l)):
-        if i != len(l) - 1:
-            print(str(l[i])+ ', ', end='')
-        else:
-            print('and ' + l[i] + '.')
-
-listToEnglish(['dogs',2,-17.5, 'cats'])
